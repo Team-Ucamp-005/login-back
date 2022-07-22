4129cb5dd92f254617c4298363480f62e25f20d6
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-07-22 18:41:07 -0500

    add middlewares

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const usersRouter = require('./user')
 const authRouter = require('./auth')
+const registerRouter = require('./register')
+const authMiddleware = require('../middlewares/authorization')
 
 router.use('/auth', authRouter)
-router.use('/users', usersRouter)
+router.use('/register', registerRouter)
+router.use(authMiddleware)
 
+router.use('/users', usersRouter)
 module.exports = router
--- src/apis/user.js
+++ src/apis/user.js
@@ -5,12 +5,31 @@ const UserService = require('../services/users')
 
 const userService = new UserService(UserModel)
 
+router.get('/me', async (req, res) => {
+  const sessionUser = req.user
+  //! Esto se hace esperando que nunca se ejecute pero es una doble validación
+  if (!sessionUser) {
+    return res.status(403).send({
+      message: 'Tu no deberías estar aquí'
+    })
+  }
+  // console.log('***********************')
+  // console.log(req.user)
+  // console.log('***********************')
+  res.send({
+    username: sessionUser.username,
+  })
+})
+
 router.post('/', async (req, res) => {
   const body = req.body
   const user = await userService.create(body)
 
   res.status(201).send(user)
 })
+
+
+
 
 
 
