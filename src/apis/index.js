e3f19087825208cfaf5b8f530f61c91ac45f60b4
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-07-23 10:50:07 -0500

    register route

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const usersRouter = require('./user')
 const authRouter = require('./auth')
+const registerRouter = require('./register')
 const authMiddleware = require('../middlewares/authorization')
 
 router.use('/auth', authRouter)
+router.use('/register', registerRouter)
 
 router.use(authMiddleware)
 router.use('/users', usersRouter)
